7dff33f56184a67f3a5710df02efbe3a4419d9f0
Tommy <twmay98@hotmail.com>
2019-09-27 13:20:37 -0700

    Add driver class

--- src/ca/bcit/comp2522/labs/lab03/Driver.java
+++ src/ca/bcit/comp2522/labs/lab03/Driver.java
@@ -1,4 +1,14 @@
 package ca.bcit.comp2522.labs.lab03;
 
+import java.util.ArrayList;
+
 public class Driver {
+    public static void main(String[] args) {
+        Planatation lotuslandLumber = new Planatation();
+        for (int i = 0; i < 10; i++) {
+            lotuslandLumber.seed();
+        }
+        ArrayList<Tree> harvested = lotuslandLumber.harvest(80);
+        System.out.println("There were " + harvested.size() + " Trees harvested.");
+    }
 }
