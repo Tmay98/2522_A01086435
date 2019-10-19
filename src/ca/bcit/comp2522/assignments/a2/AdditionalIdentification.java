ab75131210fd9adb6974d96d1103a0d8170d1aaf
Tommy <twmay98@hotmail.com>
2019-10-18 20:42:57 -0700

    Add additional id card classes

--- src/ca/bcit/comp2522/assignments/a2/AdditionalIdentification.java
+++ src/ca/bcit/comp2522/assignments/a2/AdditionalIdentification.java
@@ -9,6 +9,7 @@ package ca.bcit.comp2522.assignments.a2;
  */
 public class AdditionalIdentification extends IdentificationCard {
 
+    private String secondIDNumber;
     /**
      * Constructs an object of type AdditionalIdentification.
      *
@@ -17,11 +18,46 @@ public class AdditionalIdentification extends IdentificationCard {
      * @param cardLogo              a Boolean
      * @param cardDescription       a String
      * @param newIDNumber           a String
+     * @param newSecondIDNumber     a String
      * @param newIssueAndExpiryDate a IssueAndExpiryDate
      * @param newName               a Name
      */
     public AdditionalIdentification(String organizationName, String cardName, Boolean cardLogo, String cardDescription,
-                                    String newIDNumber, IssueAndExpiryDate newIssueAndExpiryDate, Name newName) {
+                                    String newIDNumber, String newSecondIDNumber, IssueAndExpiryDate newIssueAndExpiryDate, Name newName) {
         super(organizationName, cardName, cardLogo, cardDescription, newIDNumber, newIssueAndExpiryDate, newName);
+
+        if (newSecondIDNumber == null) {
+            throw new IllegalArgumentException("A Card's second Identification Number cannot be null.");
+        }
+        if (!isAlphaNumeric(newSecondIDNumber)) {
+            throw new IllegalArgumentException("Card second Identification Number has illegal characters.");
+        } else {
+            this.secondIDNumber = newSecondIDNumber;
+        }
+    }
+
+    /**
+     * Set the identification number of a card.
+     *
+     * @param newIdentificationNumber a String
+     */
+    public void setIdentificationNumber(String newIdentificationNumber){
+        if (newIdentificationNumber == null) {
+            throw new IllegalArgumentException("A Card's Identification Number cannot be null.");
+        }
+        if (!isAlphaNumeric(newIdentificationNumber)) {
+            throw new IllegalArgumentException("Card Identification Number has illegal characters.");
+        } else {
+            this.secondIDNumber = newIdentificationNumber;
+        }
+    }
+
+    /**
+     * Return secondIDNumber as a String.
+     *
+     * @return a String
+     */
+    public String getIdentificationNumber() {
+        return secondIDNumber;
     }
 }
--- src/ca/bcit/comp2522/assignments/a2/InsuranceCard.java
+++ src/ca/bcit/comp2522/assignments/a2/InsuranceCard.java
@@ -7,5 +7,24 @@ package ca.bcit.comp2522.assignments.a2;
  * @author Aiman Ismail
  * @version 2019
  */
-public class InsuranceCard {
+public class InsuranceCard extends AdditionalIdentification {
+
+    private String planNumber;
+
+    public InsuranceCard(String organizationName, String cardName, Boolean cardLogo, String cardDescription,
+                         String newIDNumber, String newSecondIDNumber,
+                         IssueAndExpiryDate newIssueAndExpiryDate, Name newName, String planNumber) {
+        super(organizationName, cardName, cardLogo, cardDescription, newIDNumber,
+                newSecondIDNumber, newIssueAndExpiryDate, newName);
+
+        if (planNumber == null) {
+            throw new IllegalArgumentException("A Card's second Identification Number cannot be null.");
+        }
+        if (!isAlphaNumeric(planNumber)) {
+            throw new IllegalArgumentException("Card second Identification Number has illegal characters.");
+        } else {
+            this.planNumber = planNumber;
+        }
+    }
 }
+
