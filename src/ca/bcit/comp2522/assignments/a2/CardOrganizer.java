595c48fbf8947d8d1839b2a6f6f9d946e94ac56e
Aiman Ismail <aimanismail15@gmail.com>
2019-10-18 21:17:27 -0700

    Instantiating cards

--- src/ca/bcit/comp2522/assignments/a2/CardOrganizer.java
+++ src/ca/bcit/comp2522/assignments/a2/CardOrganizer.java
@@ -25,7 +25,7 @@ public class CardOrganizer {
                 "Fukouoka-Style Tonkotsu Ramen",
                 new ContactInformation("6045598112","info@ramendanbo.com","11:00 a.m. to 11:00 p.m."),
                 "1333 Robson St., Vancouver, BC V6E 1C6");
-
+        cardList.add(danboRestaurantCard);
         // Old art Kanzando
         BusinessCard kanzandoArt = new BusinessCard("Kanzando",
                 "Old Art Kanzando",
@@ -33,6 +33,7 @@ public class CardOrganizer {
                 "A business card for art.",
                 new ContactInformation("0755610126","N/A","10:30 A.M. to 6:00 P.M."),
                 "Japanese Kanji address");
+        cardList.add(kanzandoArt);
         // Lucky Camera Shop
         BusinessCard luckyCameraShop = new BusinessCard("Lucky",
                 "Lucky Camera Shop",
@@ -40,6 +41,7 @@ public class CardOrganizer {
                 "A business card for Lucky Camera Shop",
                 new ContactInformation("0333547898", "N/A", "10:00 A.M. to 8:00 P.M."),
                 "160-0022 3-3-9 Shinjuku Shinjuku-ku Tokyo Japan");
+        cardList.add(luckyCameraShop);
         // Legendary Boot company
         BusinessCard legendaryBootCompany = new BusinessCard(
                 "Legendary Boot Company",
@@ -48,7 +50,7 @@ public class CardOrganizer {
                 "Personal Business Card for Bob McLean",
                 new ContactInformation("9154125541", "bob@legendarybootco.com", "N/A"),
                 "El Paso, Texas");
-
+        cardList.add(legendaryBootCompany);
         // BCIT Mary Wilson
         BusinessCard maryWilsonBusinessCard = new BusinessCard(
                 "BCIT",
@@ -57,6 +59,7 @@ public class CardOrganizer {
                 "Business Card for Mary Wilson",
                 new ContactInformation("6044328929", "mwilson5@bcit.ca", "N/A"),
                 "3700 Willingdon Avenue, Burnaby B.C., Canada, V5G 3H2");
+        cardList.add(maryWilsonBusinessCard);
 
     }
     public void createDiscountRewardCard() {
@@ -67,6 +70,7 @@ public class CardOrganizer {
                 true,
                 "A Discount Card for Mr Red Cafe",
                 10);
+        cardList.add(mrRedCafe);
         // Dispensary discount
         DiscountRewardCard dispensaryDiscountCard = new DiscountRewardCard(
                 "The Dispensary",
@@ -74,6 +78,7 @@ public class CardOrganizer {
                 true,
                 "A apology card with a discount",
                 10);
+        cardList.add(dispensaryDiscountCard);
     }
     public void createIdentificationCards() {
         // student card
@@ -84,6 +89,7 @@ public class CardOrganizer {
                 "A00987654",
                 new IssueAndExpiryDate("00/00/00", "04/09/09"),
                 new Name("Christopher", "G", "Thompson"));
+        cardList.add(studentCardUniversityOfBritishColumbia);
         //VPL card
         IdentificationCard vancouverPublicLibraryCard = new IdentificationCard(
                 "Vancouver Public Library",
@@ -93,6 +99,7 @@ public class CardOrganizer {
                 "",
                 new IssueAndExpiryDate("00/00/00", "00/00/00"),
                 new Name("Christopher", "G", "Thompson"));
+        cardList.add(vancouverPublicLibraryCard);
         // AlumniUBC A-Card
         IdentificationCard alumniCard = new IdentificationCard(
                 "UBC",
@@ -102,6 +109,7 @@ public class CardOrganizer {
                 "2 9424 30011 7958",
                 new IssueAndExpiryDate("00/00/00", "00/00/00"),
                 new Name("Christopher", "G", "Thompson"));
+        cardList.add(alumniCard);
         // vancouver art gallery
         IdentificationCard vancouverArtGalleryCard = new IdentificationCard(
                 "Vancouver Art Gallery",
@@ -111,7 +119,7 @@ public class CardOrganizer {
                 "79683",
                 new IssueAndExpiryDate("00/00/00", "06/18"),
                 new Name("Christopher", "G", "Thompson"));
-
+        cardList.add(vancouverArtGalleryCard);
         // MEC
         IdentificationCard mecCard = new IdentificationCard("Mountain Equipment CO-OP",
                 "MEC Card",
@@ -120,6 +128,7 @@ public class CardOrganizer {
                 "16800004",
                 new IssueAndExpiryDate("00/00/00", "00/00/00"),
                 new Name("Christopher", "G", "Thompson"));
+        cardList.add(mecCard);
         //ubc Graduate card
         IdentificationCard graduateUniversityOfBritishColumbiaCard = new IdentificationCard(
                 "UBC",
@@ -129,6 +138,7 @@ public class CardOrganizer {
                 "12345678",
                 new IssueAndExpiryDate("02/05/14", "08/31/17"),
                 new Name("Christopher", "G","Thompson") );
+        cardList.add(graduateUniversityOfBritishColumbiaCard);
         //gov't sin card
         IdentificationCard govermentSocialInsuranceNumberCard = new IdentificationCard(
                 "Employment and Immigration Canada",
@@ -138,9 +148,29 @@ public class CardOrganizer {
                 "589 588 141",
                 new IssueAndExpiryDate("00/00/00", "00/00/00"),
                 new Name("Christopher", "G", "Thompson"));
+
+        // Sunroute Card
+        IdentificationCard sunrouteClubCard = new IdentificationCard("Sunroute",
+                "Club Card",
+                true,
+                "A Club Card for Sunroute",
+                "30092459",
+                new IssueAndExpiryDate("00/00/00", "00/00/00"),
+                new Name("Christopher", "G", "Thompson"));
+        cardList.add(govermentSocialInsuranceNumberCard);
+
     }
     public void createInsuranceCard() {
-        //manulife card
+        InsuranceCard manulifeCard = new InsuranceCard(
+                "Manulife",
+                "Insurance Card",
+                true,
+                "A Insurance Card with Information",
+                "987654",
+                "0012321",
+                new IssueAndExpiryDate("00/00/00", "00/00/00"),
+                new Name("Christopher", "G", "Thompson"),
+                "9505");
     }
     public void createMembershipCard() {
         //bank of poland
@@ -149,6 +179,7 @@ public class CardOrganizer {
                 "Bank of Poland Gold Card - The peasants card.",
                 true, "A phony, novelty bank card.",
                 "Standard");
+        cardList.add(debitCardBankOfPoland);
         //independent flixx
         MembershipCard independentFlixxCard = new MembershipCard(
                 "Independent Flixx Inc.",
@@ -156,6 +187,7 @@ public class CardOrganizer {
                 true,
                 "A membership to a Independent Flixx Inc.",
                 "Standard");
+        cardList.add(independentFlixxCard);
         // lego vip card
         MembershipCard legoCard = new MembershipCard(
                 "Lego",
@@ -163,15 +195,14 @@ public class CardOrganizer {
                 true,
                 "A VIP Card for Lego",
                 "Standard");
-
-
+        cardList.add(legoCard);
         // Fairmont President's Club
         MembershipCard fairmontClubCard = new MembershipCard("Fairmont",
                 "Fairmont President's Club",
                 true,
                 "A membership card for Fairmont",
                 "Standard");
-
+        cardList.add(fairmontClubCard);
         // Black Bond Books membership card
         MembershipCard blackBondBooksCard = new MembershipCard(
                 "Black Bond Books",
@@ -179,10 +210,10 @@ public class CardOrganizer {
                 true,
                 "Membership card to Black Bond Books.",
                 "Standard");
-
+        cardList.add(blackBondBooksCard);
         // H Mart
         MembershipCard hmartDiscountCard = new MembershipCard("H Mart", "Savings Card", true, "A membership card for H Mart", "Standard");
-
+        cardList.add(hmartDiscountCard);
     }
     public void createPersonalBusinessCard() {
 
@@ -194,8 +225,7 @@ public class CardOrganizer {
                 "Personal business card for Michael Park",
                 new ContactInformation("6046815567","info@flowersbymichaelbc.ca","N/A"), new ContactInformation("6046714234","N/A","N/A"),
                 "1378 West Georgia Street, Vancouver, B.C., V6E 4S2");
-
-
+        cardList.add(flowersByMichaelPark);
     }
     public void createPointsRewardCard() {
         //Air Miles Card
@@ -208,6 +238,7 @@ public class CardOrganizer {
                 new IssueAndExpiryDate("00/00/00","06/94"),
                 new Name("Christopher", "G", "Thompson"),
                 1337);
+        cardList.add(airMilesCard);
         // plum rewards todo serial
         PointsRewardCard plumRewardsCard = new PointsRewardCard(
                 "Plum",
@@ -216,10 +247,30 @@ public class CardOrganizer {
                 "A Rewards Plum Card",
                 "500028606142", new IssueAndExpiryDate("00/00/00","00/00/00"),
                 new Name("Christopher", "G", "Thompson"), 1337);
+
+        cardList.add(plumRewardsCard);
         // sunroute club point card edy todo serial
+
         // aeroplan todo serial
+        PointsRewardCard airMilesCardTravelAndMore = new PointsRewardCard(
+                "Air Miles",
+                "Travel and More, Air Miles",
+                true,
+                "A exclusive Air Miles card.",
+                "123433",
+                new IssueAndExpiryDate("06/94", "00/00"),
+                new Name("Christopher", "G", "Thompson"),
+                1337);
         // JAL mileage bank //todo serial id number
-        // AIR MILES //todo id
+        PointsRewardCard jalMileageBank = new PointsRewardCard(
+                "JAL",
+                "Mileage Bank Card",
+                true,
+                "A points card for JAL",
+                "403954425",
+                new IssueAndExpiryDate("00/00", "00/00"),
+                new Name("Christopher", "G", "Thompson"),
+                1337);
     }
     public void createStampRewardCard() {
         StampRewardCard wholeFoodsCard = new StampRewardCard("Whole Foods",
@@ -229,16 +280,50 @@ public class CardOrganizer {
                 10,
                 3,
                 "Big ol' chicken");
+        cardList.add(wholeFoodsCard);
 
     }
     public void createTicket() {
         // studio ghibli ticket
+        Ticket ghibliTicketForHowlsMovingCastle = new Ticket("Ghibli Musuem","Ticket", true, "A ticket to the Ghibli Museum", 1000);
         // 2nd studio ghibli ticket
+        Ticket ghibliTicketForPonyoByTheCliffSide = new Ticket("Ghibli Musuem","Ticket", true, "A ticket to the Ghibli Museum", 1000);
     }
     public void createTransitCard() {
         //hello kitty
+        TransitCard helloKittyCard = new TransitCard(
+                "Suica",
+                "ICOCA",
+                true,
+                "Suica Transit Card",
+                0.0,
+                "JW301B15042825970",
+                new IssueAndExpiryDate("00/00","00/00"));
         // oyster london
+        TransitCard londonOysterTransitCard = new TransitCard(
+                "Transport for London",
+                "Oyster",
+                true,
+                "A transit card for Oyster",
+                0.0,
+                "060054294844",
+                new IssueAndExpiryDate("00/00","00/00"));
         // two japan endless discovery todo includes a id number
+        TransitCard japanEndlessDiscoveryPrimary = new TransitCard(
+                "Suica",
+                "Japan Endless Discovery",
+                true,
+                "Suica Transit Card",
+                0.0,
+                "JE338D13090202315",
+                new IssueAndExpiryDate("00/00","00/00"));
+        TransitCard japanEndlessDiscoverySecondary = new TransitCard(
+                "Suica",
+                "Japan Endless Discovery",
+                true,
+                "Suica Transit Card",
+                0.0,
+                "JE338D13090202316",
+                new IssueAndExpiryDate("00/00","00/00"));
     }
-
 }
