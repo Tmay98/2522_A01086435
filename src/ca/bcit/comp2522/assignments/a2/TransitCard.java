7d005d5855640b2b09d9c570eddd3f940ca566ca
Aiman Ismail <aimanismail15@gmail.com>
2019-10-18 21:39:56 -0700

    Instantiating cards

--- src/ca/bcit/comp2522/assignments/a2/TransitCard.java
+++ src/ca/bcit/comp2522/assignments/a2/TransitCard.java
@@ -9,7 +9,7 @@ package ca.bcit.comp2522.assignments.a2;
 public class TransitCard extends Card implements PaymentMethod {
 
     private static final int VALID_JAPAN_TRANSIT_CARD_NUMBER_LENGTH = 17;
-    private static final int VALID_LONDON_TRANSIT_CARD_NUMBER_LENGTH = 8;
+    private static final int VALID_LONDON_TRANSIT_CARD_NUMBER_LENGTH = 12;
 
     private double accountBalance;
     private String transitCardNumber;
