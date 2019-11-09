4e2db30d6528f6385c63df525e201d6f269dd7b6
Tommy <twmay98@hotmail.com>
2019-11-08 18:41:17 -0800

    add pinwheel block

--- src/ca/bcit/comp2522/assignments/a3/PinwheelBlock.java
+++ src/ca/bcit/comp2522/assignments/a3/PinwheelBlock.java
@@ -1,5 +1,10 @@
 package ca.bcit.comp2522.assignments.a3;
 
+import javafx.scene.Group;
+import javafx.scene.shape.Polygon;
+
+import java.util.ArrayList;
+
 /**
  * A quilt block with a pinwheel shaped pattern.
  *
@@ -12,9 +17,39 @@ public class PinwheelBlock extends Block {
      * Number of geometrical sections sharing the same shape and colour.
      */
     public static final int numberOfSections = 2;
+    private ArrayList<Polygon> colourGroup1 = new ArrayList<>();
+    private ArrayList<Polygon> colourGroup2 = new ArrayList<>();
+    private Group block = new Group();
 
-    private void createSections(){
+    public void createSections() {
+        //create group 1
+        Section section = new Section();
+        Polygon poly = section.createtriangle();
+        colourGroup1.add(poly);
+    }
 
-    };
+    public void createBlock() {
+        for (Polygon polygon : colourGroup1) {
+            block.getChildren().add(polygon);
+        }
+
+        for (Polygon polygon : colourGroup2) {
+            block.getChildren().add(polygon);
+        }
+    }
+
+    public ArrayList<Polygon> getColourGroup1() {
+       return colourGroup1;
+    }
+
+    public ArrayList<Polygon> getColourGroup2() {
+        return colourGroup1;
+    }
+
+    public Group getBlock() {
+        return block;
+    }
+
     private void blockColour(String colourOne, String colourTwo){};
+
 }
--- src/ca/bcit/comp2522/assignments/a3/QuiltProgram.java
+++ src/ca/bcit/comp2522/assignments/a3/QuiltProgram.java
@@ -1,5 +1,11 @@
 package ca.bcit.comp2522.assignments.a3;
+import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 /**
  * Program to generate a quilt.
@@ -8,7 +14,24 @@ import javafx.scene.Scene;
  * @version 2019
  */
 
-public class QuiltProgram {
+public class QuiltProgram extends Application {
     private Quilt quilt;
     private Scene scene;
+
+    public void start(Stage primaryStage) {
+        Section section = new Section();
+        PinwheelBlock test = new PinwheelBlock();
+        test.createSections();
+        test.createBlock();
+        Rectangle ground = new Rectangle(0, 250, 500, 100);
+        ground.setFill(Color.BURLYWOOD);
+
+        // creating root
+        Group root = new Group(test.getBlock());
+        Scene scene = new Scene(root, 500, 350, Color.LIGHTBLUE);
+
+        primaryStage.setTitle("Tommy");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
