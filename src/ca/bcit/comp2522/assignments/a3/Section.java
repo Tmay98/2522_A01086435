0f44c79fddde6c72a02d5249c7014b185754e837
fish <frances.mach@gmail.com>
2019-11-08 18:32:35 -0800

    parallelogram

--- src/ca/bcit/comp2522/assignments/a3/Section.java
+++ src/ca/bcit/comp2522/assignments/a3/Section.java
@@ -13,18 +13,43 @@ import javafx.scene.shape.Rectangle;
 public class Section {
     private String colour;
 
-    public Polygon createtriangle() {
+    /**
+     * Instantiates a 90 degree triangle.
+     *
+     * @return new Polygon a Polygon
+     */
+    public Polygon createTriangle() {
         return new Polygon(
                 50, 0,
                 0, 50,
                 100, 50
         );
     }
+    /**
+     * Instantiates a square.
+     *
+     * @return  new rectangle a Rectangle
+     */
+    public Rectangle createSquare() {
+        return new Rectangle(
+                0, 0,
+                50, 50
+        );
+    }
+    /**
+     * Instantiates a parallelogram.
+     *
+     * @return  paralleogram a Rectangle
+     */
+    public Rectangle createParallelogram() {
+        Rectangle parallelogram = new Rectangle(0, 0, 100, 50);
 
-    private Rectangle rectangle = new Rectangle(
-            0, 0,
-            40, 40
-    );
+        Shear paralellogramPivot = new Shear();
+        paralellogramPivot.setPivotY(45);
+        parallelogram.getTransforms().add(paralellogramPivot);
+
+        return parallelogram;
+    }
 
     
 
