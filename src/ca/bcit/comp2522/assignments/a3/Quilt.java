4c7796007e9b7a5e1feda86631f03a1b0e5b5ebd
Tommy <twmay98@hotmail.com>
2019-11-13 12:45:18 -0800

    Add Quilt constructor. Add first scene to QuiltProgram.

--- src/ca/bcit/comp2522/assignments/a3/Quilt.java
+++ src/ca/bcit/comp2522/assignments/a3/Quilt.java
@@ -15,6 +15,53 @@ public class Quilt {
     private int blockLength;
     private ArrayList<Block> blocks;
 
+    /**
+     * Constructs an object of type Quilt.
+     */
+    public Quilt() {
+        this.numberOfBlocks = 0;
+        this.rows = 0;
+        this.columns = 0;
+        this.blockLength = 100;
+        this.blocks = new ArrayList<>();
+    }
+
+    /**
+     * returns the number of rows in the quilt.
+     *
+     * @return rows
+     */
+    public int getRows() {
+        return rows;
+    }
+
+    /**
+     * returns the number of columns in the quilt.
+     *
+     * @return columns
+     */
+    public int getColumns() {
+        return columns;
+    }
+
+    /**
+     * Sets the number of rows in the quilt.
+     *
+     * @param rows an int
+     */
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    /**
+     * Sets the number of columns in the quilt.
+     *
+     * @param columns an int
+     */
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
     private void addBlock(Block block){};
     private void removeBlock(Block block){};
     public void createQuilt(){};
--- src/ca/bcit/comp2522/assignments/a3/QuiltProgram.java
+++ src/ca/bcit/comp2522/assignments/a3/QuiltProgram.java
@@ -1,11 +1,15 @@
 package ca.bcit.comp2522.assignments.a3;
-import javafx.application.Application;
-import javafx.scene.Group;
+
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 /**
  * Program to generate a quilt.
@@ -13,25 +17,215 @@ import javafx.stage.Stage;
  * @author Frances Mach & Tommy May
  * @version 2019
  */
-
-public class QuiltProgram extends Application {
+public class QuiltProgram {
     private Quilt quilt;
-    private Scene scene;
+    private ArrayList<Scene> scenes;
+    private int blockSize;
+    private Stage stage;
 
-    public void start(Stage primaryStage) {
-        HourglassBlock block = new HourglassBlock();
-        block.createSections();
+    /**
+     * Constructs an object of type QuiltProgram.
+     *
+     * @param stage a Stage
+     */
+    public QuiltProgram(Stage stage) {
+        this.blockSize = 0;
+        this.quilt = new Quilt();
+        this.stage = stage;
+        this.scenes = new ArrayList<>();
+        createSceneOne();
+        createSceneTwo();
+    }
 
-        PinwheelBlock test = new PinwheelBlock();
-        Rectangle ground = new Rectangle(0, 250, 500, 100);
-        ground.setFill(Color.BURLYWOOD);
+    /**
+     * Returns the stage.
+     *
+     * @return stage
+     */
+    public Stage getStage() {
+        return stage;
+    }
 
-        // creating root
-        Group root = new Group(block.getBlock());
-        Scene scene = new Scene(root, 500, 350, Color.LIGHTBLUE);
+    /**
+     * returns the scenes ArrayList.
+     *
+     * @return scenes
+     */
+    public ArrayList<Scene> getScenes() {
+        return scenes;
+    }
 
-        primaryStage.setTitle("Tommy");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    /**
+     * Creates the first scene and adds it to the scenes ArrayList.
+     */
+    private void createSceneOne() {
+        Text title = new Text("Quilt settings");
+        title.setFont(Font.font(30));
+        title.setFill(Color.RED);
+        StackPane titlePane = new StackPane(title);
+        titlePane.setStyle("-fx-padding: 20px; -fx-background-color: black");
+
+        // create radio buttons
+        VBox buttons = createRadioButtonGroup();
+
+        // create spinners
+        VBox spinners = createIntegerSpinnersGroup();
+
+        // add selector controls to vbox
+        VBox selectorControls =
+                new VBox(buttons, spinners);
+        selectorControls.setStyle("-fx-padding: 40px 45px; " + "-fx-background-color: skyblue");
+        selectorControls.setSpacing(20);
+
+        // create borderPane
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(titlePane);
+        borderPane.setLeft(selectorControls);
+
+        // add first scene to ArrayList
+        scenes.add(new Scene(borderPane, 800, 600));
+    }
+
+    /**
+     * Creates a VBox with all the integer spinners.
+     *
+     * @return spinners
+     */
+    private VBox createIntegerSpinnersGroup() {
+        // Add integer spinner for selecting number of blocks
+        SpinnerValueFactory.IntegerSpinnerValueFactory svf1 =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(
+                        1, 100, 5);
+        SpinnerValueFactory.IntegerSpinnerValueFactory svf2 =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(
+                        1, 100, 5);
+        SpinnerValueFactory.IntegerSpinnerValueFactory svf3 =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(
+                        1, 100, 5);
+        Spinner<Integer> blockSizeSpinner = new Spinner<>(svf1);
+        Spinner<Integer> rowsSpinner = new Spinner<>(svf2);
+        Spinner<Integer> columnsSpinner = new Spinner<>(svf3);
+
+        // add labels for integer spinners
+        Label blockSizeLabel = new Label("Block size (cm)");
+        blockSizeLabel.setScaleX(1.8);
+        blockSizeLabel.setScaleY(1.8);
+
+        Label columnsLabel = new Label("Select Columns");
+        columnsLabel.setScaleX(1.8);
+        columnsLabel.setScaleY(1.8);
+
+        Label rowsLabel = new Label("Select Rows");
+        rowsLabel.setScaleX(1.8);
+        rowsLabel.setScaleY(1.8);
+
+        // add next scene button
+        Button nextSceneButton = new Button("Next");
+        nextSceneButton.setOnAction((event) -> {
+            this.quilt.setColumns(columnsSpinner.getValue());
+            this.quilt.setRows(rowsSpinner.getValue());
+            this.blockSize = blockSizeSpinner.getValue();
+            stage.setScene(scenes.get(1));
+        });
+
+        // Add spinners to VBox and return
+        VBox spinners = new VBox(rowsLabel, rowsSpinner,
+                columnsLabel, columnsSpinner,
+                blockSizeLabel, blockSizeSpinner,
+                nextSceneButton);
+        spinners.setSpacing(20);
+        return spinners;
+    }
+
+    /**
+     * Creates a VBox with all the radio buttons.
+     *
+     * @return buttons
+     */
+    private VBox createRadioButtonGroup() {
+        // radio buttons for single or multi block quilt
+        ToggleGroup blockType = new ToggleGroup();
+        RadioButton singleButton = new RadioButton("Single Block");
+        RadioButton multiButton = new RadioButton("Multi Block");
+        singleButton.setScaleX(2);
+        singleButton.setScaleY(2);
+        multiButton.setScaleX(2);
+        multiButton.setScaleY(2);
+        singleButton.setToggleGroup(blockType);
+        multiButton.setToggleGroup(blockType);
+
+        // set onclick events
+        singleButton.setOnAction((event) -> createSingleQuilt());
+        multiButton.setOnAction((event) -> createMultiQuilt());
+
+        // add radio buttons label
+        Label buttonLabel = new Label("select block type");
+        buttonLabel.setScaleX(1.8);
+        buttonLabel.setScaleY(1.8);
+
+        // add to VBox and return
+        VBox buttons = new VBox(buttonLabel, singleButton, multiButton);
+        buttons.setSpacing(20);
+        return buttons;
+    }
+
+    /**
+     * Creates the secons scene and adds it to the scenes ArrayList.
+     */
+    private void createSceneTwo() {
+        Text title = new Text("Scene two");
+        title.setFont(Font.font(30));
+        title.setFill(Color.RED);
+
+        StackPane titlePane = new StackPane(title);
+        titlePane.setStyle("-fx-padding: 20px; -fx-background-color: black");
+
+        ToggleGroup group = new ToggleGroup();
+        RadioButton singleButton = new RadioButton("Single Block");
+        RadioButton multiButton = new RadioButton("Multi Block");
+        singleButton.setToggleGroup(group);
+        multiButton.setToggleGroup(group);
+        singleButton.setOnAction((event) -> createSingleQuilt());
+        multiButton.setOnAction((event) -> createMultiQuilt());
+        singleButton.setScaleX(2);
+        singleButton.setScaleY(2);
+        multiButton.setScaleX(2);
+        multiButton.setScaleY(2);
+
+        Separator separator = new Separator();
+
+        Label buttonLabel = new Label("select block type");
+        buttonLabel.setScaleX(1.8);
+        buttonLabel.setScaleY(1.8);
+
+        Button nextSceneButton = new Button("Next");
+        nextSceneButton.setOnAction((event) -> stage.setScene(scenes.get(0)));
+        VBox selectorControls =
+                new VBox(buttonLabel, singleButton, multiButton, separator, nextSceneButton);
+        selectorControls.setStyle("-fx-padding: 40px 45px; " + "-fx-background-color: skyblue");
+        selectorControls.setSpacing(25);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(titlePane);
+        borderPane.setLeft(selectorControls);
+
+        // add first scene to ArrayList
+        scenes.add(new Scene(borderPane, 800, 600));
+    }
+
+    /**
+     * Creates a singleQuilt.
+     */
+    private void createSingleQuilt() {
+        this.quilt = null;
+        this.quilt = new SingleBlockQuilt();
+    }
+
+    /**
+     * Creates a multiQuilt.
+     */
+    private void createMultiQuilt() {
+        this.quilt = null;
+        this.quilt = new MultiBlockQuilt();
     }
 }
