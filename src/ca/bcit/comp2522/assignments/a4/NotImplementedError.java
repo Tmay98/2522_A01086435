658d95474c1fb7772fa112ca969e467efd8aa3c2
Tommy <twmay98@hotmail.com>
2019-11-22 16:25:42 -0800

    add files

---
+++ src/ca/bcit/comp2522/assignments/a4/NotImplementedError.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2522.assignments.a4;
+
+/**
+ * NotImplementedError
+ *
+ * @author Your name
+ * @author Your name
+ * @version Date
+ */
+public class NotImplementedError extends Exception {
+
+}
