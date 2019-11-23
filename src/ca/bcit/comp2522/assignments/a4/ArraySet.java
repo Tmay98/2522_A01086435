139f437325d60cc51d020455fe0d7d1f28a765fc
Tommy <twmay98@hotmail.com>
2019-11-22 16:56:55 -0800

    implement add

--- src/ca/bcit/comp2522/assignments/a4/ArraySet.java
+++ src/ca/bcit/comp2522/assignments/a4/ArraySet.java
@@ -79,7 +79,10 @@ public class ArraySet<E> implements Set<E>, MyIterable<E> {
      */
     public boolean add(final E element) {
         // Your code goes here
-        return false;
+        if (element == null) { return false; }
+        if (this.contains(element)) { return false; }
+        collection[elementCount++] = element;
+        return true;
     }
 
     /**
