c2996141af40b48c0096590fa0a063a9ab4f4d56
fish <frances.mach@gmail.com>
2019-12-04 15:23:41 -0800

    visit node

--- src/ca/bcit/comp2522/labs/lab12/Graph.java
+++ src/ca/bcit/comp2522/labs/lab12/Graph.java
@@ -2,20 +2,42 @@ package ca.bcit.comp2522.labs.lab12;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a Graph.
+ *
+ * @author Tommy May & Frances Mach
+ * @version 2019
+ */
 public class Graph {
     private ArrayList<Node> nodes;
+    private ArrayList<Node> stack;
 
+    /**
+     * Instantiates a graph object.
+     */
     public Graph() {
         nodes = new ArrayList<>();
     }
-
+    /**
+     * Sets the nodes of the graph.
+     * @param nodes an ArrayList of nodes
+     */
     public void setNodes(ArrayList<Node> nodes) {
         this.nodes = nodes;
     }
-
+    /**
+     * Returns an ArrayList of nodes.
+     * @return nodes an ArrayList
+     */
     public ArrayList<Node> getNodes() {
         return nodes;
     }
-
+    /**
+     * Visits a node and puts it on the stack.
+     * @param node a Node
+     */
+    private void visitNode(Node node) {
+        stack.add(node);
+    }
     
 }
