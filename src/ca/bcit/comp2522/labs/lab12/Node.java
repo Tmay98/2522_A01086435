c3253651fe00e0654499cb9d37da20b31e83d78c
Tommy <twmay98@hotmail.com>
2019-12-04 15:00:08 -0800

    toArray returns an Object array

--- src/ca/bcit/comp2522/labs/lab12/Node.java
+++ src/ca/bcit/comp2522/labs/lab12/Node.java
@@ -3,11 +3,21 @@ package ca.bcit.comp2522.labs.lab12;
 import java.util.ArrayList;
 
 public class Node {
-    ArrayList<Boolean> links;
+    private ArrayList<Integer> links;
 
+    public ArrayList<Integer> getLinks() {
+        return links;
+    }
 
     public Node(String connections) {
         links = new ArrayList<>();
-        
+        for (int i = 0; i < connections.length(); i++) {
+            links.add(Integer.parseInt(connections.substring(i, i + 1)));
+        }
+    }
+
+    public static void main(String[] args) {
+        Node test = new Node("0101");
+        System.out.println(test.links);
     }
 }
--- src/ca/bcit/comp2522/labs/lab12/main.java
+++ src/ca/bcit/comp2522/labs/lab12/main.java
@@ -3,6 +3,7 @@ package ca.bcit.comp2522.labs.lab12;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
@@ -11,16 +12,19 @@ public class main {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter name of text file");
         String input = in.nextLine();
-        readFile(input);
+        ArrayList<Node> nodes = readFile(input);
+        System.out.println(nodes.get(0).getLinks());
     }
 
-    static String readFile(String path) throws IOException {
+    static ArrayList<Node> readFile(String path) throws IOException {
+        ArrayList<Node> nodes = new ArrayList<>();
         try (BufferedReader br =
                      new BufferedReader(new FileReader(path))) {
             String st;
             while((st = br.readLine()) != null) {
-
+                nodes.add(new Node(st));
             }
+            return nodes;
         }
     }
 
