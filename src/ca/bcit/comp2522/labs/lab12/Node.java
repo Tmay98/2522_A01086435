fc0c33585ce3ced887b31c7e972c67ff3e3da1b2
fish <frances.mach@gmail.com>
2019-12-06 15:39:55 -0800

    clean up nodes

--- src/ca/bcit/comp2522/labs/lab12/Node.java
+++ src/ca/bcit/comp2522/labs/lab12/Node.java
@@ -9,32 +9,53 @@ import java.util.ArrayList;
  * @version 2019
  */
 public class Node {
-    int nodeValue;
+    private int nodeValue;
     private ArrayList<Integer> links;
     private boolean visited;
 
+    /**
+     * Instantiates a node object from a String, and it's integer value.
+     * @param nodeString a node and it's links in String form.
+     * @param nodeValue an int
+     */
+    public Node(String nodeString, int nodeValue) {
+        links = new ArrayList<>();
+        for (int i = 0; i < nodeString.length(); i++) {
+            links.add(Integer.parseInt(nodeString.substring(i, i + 1)));
+        }
+        this.visited = false;
+        this.nodeValue = nodeValue;
+    }
+
+    /**
+     * Returns the value stored inside a Node.
+     * @return nodeValue an int
+     */
     public int getNodeValue() {
         return nodeValue;
     }
 
+    /**
+     * Returns the links to other nodes in a Node.
+     * @return links an ArrayList of int
+     */
     public ArrayList<Integer> getLinks() {
         return links;
     }
 
+    /**
+     * Returns the visitation state of a node.
+     * @return visited a boolean
+     */
     public boolean isVisited() {
         return visited;
     }
 
+    /**
+     * Sets the visitation state of a node.
+     * @param visited a boolean
+     */
     public void visit(boolean visited) {
         this.visited = visited;
-    }
-
-    public Node(String connections, int nodeValue) {
-        links = new ArrayList<>();
-        for (int i = 0; i < connections.length(); i++) {
-            links.add(Integer.parseInt(connections.substring(i, i + 1)));
-        }
-        this.visited = false;
-        this.nodeValue = nodeValue;
     }
 }
